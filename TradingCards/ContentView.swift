e46ebc78cd0cceae2f89fefcffbb3e03c02470f4
Leon Gell <lgell@lcs.on.ca>
2023-01-12 12:17:15 -0500

    Name and picture of starting structure in

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                    // first color background
+                    Color("DarkBlue")
+                
+                
+                VStack{
+                    Text("Auston Matthews")
+                        .font(Font.custom("HelveticaNeue-Medium", size: 40))
+                    Image("Matthews")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 400)
+                    
+                    
+                    
+                        
+                }
+                
+            }
+           
+        
+            
+            VStack {
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
