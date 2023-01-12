b5253060036c190eb2fb5f29989cffeb5859fd5d
Leon Gell <lgell@lcs.on.ca>
2023-01-12 13:35:12 -0500

    Finished player card (Need to wait for response of Gordon for bubble around image.)

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -9,32 +9,83 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            ZStack {
+        ScrollView {
+            VStack {
+                ZStack {
                     // first color background
                     Color("DarkBlue")
-                
-                
-                VStack{
-                    Text("Auston Matthews")
-                        .font(Font.custom("HelveticaNeue-Medium", size: 40))
-                    Image("Matthews")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 300, height: 400)
                     
                     
-                    
+                    //Name
+                    VStack{
+                        Text("Auston Matthews")
+                            .font(Font.custom("HelveticaNeue-Medium", size: 40))
+                        //image
+                        Image("Matthews")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 400)
+                    //.background(RoundedRectangle(cornerRadius: 0)
+                                    // Create the shape
+                            //.foregroundColor(Color.white))
                         
+                    }
+                    //Logo placement
+                    VStack {
+                    Spacer()
+                            .frame(height: 350)
+                    HStack{
+                        Spacer()
+                            .frame(width: 175)
+                        Image("MapleLeafs")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                        
+                    }
+                }
+                    //Number Placement
+                    VStack {
+                        HStack {
+                            Spacer()
+                                .frame(width: 220)
+                           
+                            Text("#34")
+                            
+                                .font(Font.custom("HelveticaNeue-Medium", size: 40))
+                        }
+                        Spacer()
+                        .frame(height: 290)
+                    }
+                    //.background(RoundedRectangle(cornerRadius: 0)
+                                // Create the shape
+                        //.foregroundColor(Color.white))
+                  
+                    //National Country
+                    VStack {
+                        Spacer()
+                            .frame(height: 430)
+                        HStack{
+                            
+                          Image("USA")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 100)
+                            Spacer()
+                                .frame(width: 200)
+                        }
+                        
+                        
+                    }
+                    
                 }
                 
+                
+                
+                
+               
+                
             }
-           
-        
-            
-            VStack {
-            }
-            .padding()
         }
     }
 }
